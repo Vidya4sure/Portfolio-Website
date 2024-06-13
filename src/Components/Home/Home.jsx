606b6314ef70ec59b9a88e1f43b566a2cb384635
vidya <hansdavsh@gmail.com>
2024-06-13 05:33:05 +0530

    implement ReactRouter and add header

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import VidyaProfileImg from '../assets/vidya profile img.png'
+import Skills from '../smallComp/Skills'
+import Box from '../smallComp/Box'
+import Socials from '../smallComp/Socials'
+
+const Home = () => {
+  return (
+    <>
+    <div className="md:w-1/3 m-auto">
+      <div className="mt-2 w-96 sm:w-3/4 md:w-full m-auto rounded bg-sky-500 hover:bg-sky-600 hover:text-white cursor-pointer">
+        <h1 className="text-2xl py-5 px-5  text-center">Hello I'm Vidya Sagar</h1>
+      </div>
+
+      <div className="mt-2 flex justify-between">
+        <div className="pt-4 ml-1">
+          <h1 className="text-2xl font-semibold">Vidya Sagar Hansda</h1>
+          <h2 className="text-xl py-2">Aspiring <span className="bg-green-700 px-2 text-white rounded text-center">MERN</span> stack developer</h2>
+        </div>
+        <div className="w-32 mr-1">
+          <img src={VidyaProfileImg} alt="Vidya profile pic" />
+        </div>
+      </div>
+
+      <div>
+        <h1 className="text-2xl mt-2 underline decoration-solid">About</h1>
+        <p className="pt-2 text-lg">Hello! 👋 I am <span className="underline">Vidya Sagar</span> a 2nd year undergrad at Marwari College Ranchi , Ranchi Jharkhand. I enjoy making a simple yet beautiful webpages. I also love designing.When I’m not coding , you’ll find me creating UI designs.</p>
+      </div>
+
+      <div>
+        <h1 className="text-2xl mt-2 underline decoration-solid">Skills</h1>
+        <div className="flex flex-wrap p-1">
+          <Skills skills={"Javascript"}></Skills>
+          <Skills skills={"Tailwindcss"}></Skills>
+          <Skills skills={"React.js"}></Skills>
+          <Skills skills={"Node.js"}></Skills>
+          <Skills skills={"Express.js"}></Skills>
+          <Skills skills={"MongoDB"}></Skills>
+        </div>
+      </div>
+
+      <div className="flex gap-2 justify-center ml-2 sm:ml-2 md:ml-2 lg:ml-0">
+        <Box  inputs="Projects"></Box>
+        <Box inputs="Resume"></Box>
+      </div>
+
+      <div>
+      <h1 className="text-2xl underline decoration-solid">Socials</h1>
+        <Socials links={"https://github.com/Vidya4sure"} socials={"Github"}></Socials>
+        <Socials links={"https://www.linkedin.com/in/vidya4sure/"} socials={"LinkedIn"}></Socials>
+        <Socials links={"https://x.com/vidya4sure"} socials={"X"}></Socials>
+      </div>
+      <div className="flex justify-center mt-56 text-gray-500">
+        <h3>© 2024 vidya4sure. All Rights Reserved.</h3>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import { createBrowserRouter , createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Contact from './Components/Contact/Contact.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+    </Route>
+   )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
